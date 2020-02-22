050e368d816de9280dec242af4528c8780805d78
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-02-22 21:39:49 +0200

    update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <section class="search-form">
     <form class="form-inline">
-        <input id="s" name="s" class="form-control" type="text" placeholder="<?php _e('Поиск', 'aletheme'); ?>" value="<?php echo get_search_query(); ?>">
-         <button class="btn-submit" type="submit" value="<?php _e('Search', 'zlv')?>"><i class="fas fa-search"></i></button>
+        <input id="s" name="s" class="form-control" type="text" placeholder="<?php _e('Поиск', 'zlv'); ?>" value="<?php echo get_search_query(); ?>">
+         <button class="btn-submit" type="submit" value="<?php _e('Поиск', 'zlv')?>"><i class="fas fa-search"></i></button>
     </form> 
 </section>
