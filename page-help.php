78999b9543214da53c8ea54de89469492b7c9465
Havryliuk-Serhii <havserhii@gmail.com>
2020-02-29 22:07:01 +0200

    finished page-help.php

--- page-help.php
+++ page-help.php
@@ -22,13 +22,13 @@ Template Name: Page Help
                             <div class="col-lg-6 col-md-12">
                                 <div class="tab-content">
                                     <div role="tabpanel" class="tab-pane active" id="housing">
-                                        <img src="img/DogHotel.jpg" alt="">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/DogHotel.jpg" alt="">
                                     </div>
                                     <div role="tabpanel" class="tab-pane" id="feed">
-                                        <img src="img/Dog-cat-Organic.jpg" alt="">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Dog-cat-Organic.jpg" alt="">
                                     </div>
                                     <div role="tabpanel" class="tab-pane" id="medicines">
-                                        <img src="img/contfile.jpg" alt="">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/contfile.jpg" alt="">
                                     </div>
                                 </div>
                             </div>
@@ -38,13 +38,13 @@ Template Name: Page Help
                                         <div class="card-header" id="headingOne" role="presentation">
                                             <a href="#housing" role="tab" data-toggle="tab" >
                                                 <div class="mb-0">
-                                                    <div role="link" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Передержка животных</div>
+                                                    <div role="link" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"><?php the_field('first_tab_title'); ?></div>
                                                 </div>
                                             </a>
                                         </div>
-                                        <div id="collapseOne" class="collapse show active" aria-labelledby="headingOne" data-parent="#accordionExample">                              
+                                        <div id="collapseOne" class="collapse show active" aria-labelledby="headingOne" data-parent="#accordionExample">  
                                             <div class="card-body single-feature">                                
-                                                <p>У нас очень много животных, но мало места. Мы вынуждены просить друзей и знакомых разместить на некоторое время у себя хотя-бы пару хвостов.</p>
+                                                <?php the_field('first_tab_content'); ?>
                                             </div>                                            
                                         </div>
                                     </div>
@@ -52,13 +52,13 @@ Template Name: Page Help
                                         <div class="card-header" id="headingTwo" role="presentation">
                                             <a href="#feed" role="tab" data-toggle="tab">
                                                 <div class="mb-0">
-                                                    <div role="link" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Корма для животных</div>
+                                                    <div role="link" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"><?php the_field('second_tab_title'); ?></div>
                                                 </div>
                                             </a>
                                         </div>
                                         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                                             <div class="card-body single-feature">                               
-                                                <p>К нам постоянно попадают все новые и новые жильцы у которых отменный аппетит вызванный уличным недоеданием.</p>
+                                                <?php the_field('second_tab_content'); ?>
                                             </div>
                                         </div>
                                     </div>
@@ -66,13 +66,13 @@ Template Name: Page Help
                                         <div class="card-header" id="headingThree" role="presentation">
                                             <a href="#medicines" role="tab" data-toggle="tab">
                                                 <div class="mb-0">
-                                                    <div role="link" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Лекарства для животных</div>
+                                                    <div role="link" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"><?php the_field('third_tab_title'); ?></div>
                                                 </div>
                                             </a>
                                         </div>
-                                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">                                          
+                                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                                             <div class="card-body single-feature">
-                                                <p>Всех животных мы лечим и обязательно обрабатываем от блох, глистов и стерилизуем.</p>
+                                                <?php the_field('third_tab_content'); ?>
                                             </div>                                            
                                         </div>
                                     </div>
@@ -83,7 +83,7 @@ Template Name: Page Help
         <div class="row">
             <div class="col-md-12">
                 <div class="help-action">
-                    <p class="lead">Средства просим перечислять на карту Приватбанка №12345 на имя ....<br>Если у Вас есть возможность помочь нам помещением, едой и лекарствами для животных то обращайтесь по телефонам и адресам на странице Контакты</p>
+                    <p class="lead"><?php the_field('help_action_content'); ?></p>
                 </div>
             </div>
         </div>
